2676b545140fc32e59b4197cb975360ecb4ea1ff
imsoling <eumsoli@gmail.com>
2024-03-26 22:59:04 +0900

    Level0 : Day1

---
+++ PROGRAMMERS/src/Level0/Day1_004.java
@@ -0,0 +1,20 @@
+package Level0;
+
+public class Day1_004 {
+
+    public static void main(String[] args) {
+
+        solution(1,2);
+
+        System.out.println(solution(1,2));
+    }
+
+    public static int solution(int a, int b) {
+        int num1 = a;
+        int num2 = b;
+
+        int answer = num1 / num2;
+
+        return answer;
+    }
+}
