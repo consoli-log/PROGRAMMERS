b46ad46929bc1928dfb3978dfe84d71895d9803f
soli.out.println <eumsoli@gmail.com>
2024-06-19 11:46:54 +0900

    Day2

--- PROGRAMMERS/src/Level0/Day2_003.java
+++ PROGRAMMERS/src/Level0/Day2_003.java
@@ -2,7 +2,16 @@ package Level0;
 
 import java.util.Arrays;
 
-// 분수의 덧셈
+    /*
+    @ 분수의 덧셈
+
+    첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1,
+    두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다.
+    두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+    @ 제한사항
+    - 0 <numer1, denom1, numer2, denom2 < 1,000
+    */
 public class Day2_003 {
 
     public static void main(String[] args) {
--- PROGRAMMERS/src/Level0/Day2_004.java
+++ PROGRAMMERS/src/Level0/Day2_004.java
@@ -2,7 +2,16 @@ package Level0;
 
 import java.util.Arrays;
 
-// 배열 두 배 만들기
+    /*
+    @ 배열 두 배 만들기
+
+    정수 배열 numbers가 매개변수로 주어집니다.
+    numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+
+    @ 제한사항
+    - -10,000 ≤ numbers의 원소 ≤ 10,000
+    - 1 ≤ numbers의 길이 ≤ 1,000
+    */
 public class Day2_004 {
 
     public static void main(String[] args) {
