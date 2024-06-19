28222fc1a92f78dca7f075fd11f3a7700c979ce1
soli.out.println <eumsoli@gmail.com>
2024-06-19 11:47:20 +0900

    Day13

--- PROGRAMMERS/src/Level0/Day13_002.java
+++ PROGRAMMERS/src/Level0/Day13_002.java
@@ -2,7 +2,16 @@ package Level0;
 
 import java.util.Arrays;
 
-// 배열 원소의 길이
+    /*
+    @ 배열 원소의 길이
+
+    문자열 배열 strlist가 매개변수로 주어집니다.
+    strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+
+    @ 제한사항
+    - 1 ≤ strlist 원소의 길이 ≤ 100
+    - strlist는 알파벳 소문자, 대문자, 특수문자로 구성되어 있습니다.
+    */
 public class Day13_002 {
 
     public static void main(String[] args) {
